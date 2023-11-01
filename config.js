bcb6b63328288c3acd3353e3d765a96dbb005983
Montsi Phooko <58219036+jabariwoods@users.noreply.github.com>
2023-11-01 03:27:58 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Database configuration
+  database: {
+    host: 'localhost',
+    port: 3306,
+    username: 'your_username',
+    password: 'your_password',
+    database: 'your_database_name',
+  },
+  
+  // Server configuration
+  server: {
+    port: 3000,
+    host: 'localhost',
+  },
+  
+  // API keys or secrets
+  apiKeys: {
+    secretKey: 'your_secret_key',
+    otherApiKey: 'other_api_key',
+  },
+};
